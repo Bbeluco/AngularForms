2129c3789d7fde87de103f8d4eb6b680e9aeb615
Bruno Beluco <beluco.bispo@hotmail.com>
2024-05-26 10:52:30 -0300

    feat: first validation name field

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,38 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  formulario = new FormGroup({
+    nome: new FormControl('', [
+      Validators.required
+    ]),
+    nascimento: new FormControl(''),
+    telefone: new FormControl(''),
+    email: new FormControl(''),
+    cep: new FormControl(''),
+    endereco: new FormControl(''),
+    numero: new FormControl(''),
+    complemento: new FormControl(''),
+    bairro: new FormControl(''),
+    cidade: new FormControl('')
+  })
+
+  ngOnInit(): void {
+  }
+
+  cadastrar(){
+      if(this.formulario.status == "VALID") {
+        this.router.navigate(['./sucesso'])
+      }
+      console.log('Formulário enviado');
+  }
+}
